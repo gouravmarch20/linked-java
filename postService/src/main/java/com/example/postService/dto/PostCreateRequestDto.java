416040dd19e8afe2f0c7154f30f9f6f87bb43f83
Sourabh-km13 <lordgk02@gmail.com>
2026-07-28 00:28:58 +0530

    se3tup-3

--- postService/src/main/java/com/example/postService/dto/PostCreateRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.postService.dto;
-import lombok.Data;
-
-@Data
-public class PostCreateRequestDto {
-    private String content;
-}
--- postService/src/main/java/com/example/postService/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.postService.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
